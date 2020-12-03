2dc94906338512700c93d244dee3c1a2fe1fc086
Simon Adorf <simon.adorf@epfl.ch>
2020-12-03 14:04:53 +0100

    Use 'helm upgrade' for deployment.

--- combine.py
+++
@@ -1,9 +0,0 @@
-import hiyapyco
-
-with open('./jupyterhub/values.yaml.orig') as fp:
-    values = fp.read()
-with open('./config.yaml') as fp:
-    config = fp.read()
-
-merged_yaml = hiyapyco.load([values, config], method=hiyapyco.METHOD_MERGE)
-print(hiyapyco.dump(merged_yaml))
