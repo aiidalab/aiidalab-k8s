6d01f706f6e4c60823b098740dd29d8b4a58c833
Simon Adorf <simon.adorf@epfl.ch>
2021-06-07 20:10:27 +0200

    update autoscaler policy and role name.

--- terraform/aws/autoscaler.tf
+++ terraform/aws/autoscaler.tf
@@ -7,7 +7,7 @@ module "iam_assumable_role_admin" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "~> v3.8.0"
   create_role                   = true
-  role_name                     = "cluster-autoscaler-blogpost"
+  role_name                     = "cluster-autoscaler-aiidalab"
   provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
   role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:cluster-autoscaler-aws-cluster-autoscaler"]
@@ -34,6 +34,8 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
       "autoscaling:DescribeAutoScalingInstances",
       "autoscaling:DescribeLaunchConfigurations",
       "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
       "ec2:DescribeLaunchTemplateVersions",
     ]
 
